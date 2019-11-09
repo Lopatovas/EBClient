cf183907628adfa30ec192b607ca33065558cd19
Lopatovas <lopatovas@gmail.com>
2019-11-09 22:41:13 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Client side for EB
-
-
---
+++ precache-manifest.1f3fd5a8e0cea6a32cc27e4331fd38c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be9c9d94faa4c5ac759069ca7f7a146",
+    "url": "/EBClient/index.html"
+  },
+  {
+    "revision": "b64ea9ef495672b4662e",
+    "url": "/EBClient/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "6e53bb05fa93210a6a07",
+    "url": "/EBClient/static/css/main.633707eb.chunk.css"
+  },
+  {
+    "revision": "b64ea9ef495672b4662e",
+    "url": "/EBClient/static/js/2.1f3086a9.chunk.js"
+  },
+  {
+    "revision": "6e53bb05fa93210a6a07",
+    "url": "/EBClient/static/js/main.787fe254.chunk.js"
+  },
+  {
+    "revision": "38397139612274807990",
+    "url": "/EBClient/static/js/runtime-main.3501e20a.js"
+  }
+]);
